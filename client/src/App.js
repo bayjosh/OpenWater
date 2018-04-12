86c2dd28cbfdc1891f95fb35f41931a78c31563a
unknown <arpeterson78@gmail.com>
2018-04-12 12:53:51 -0500

    homepage styling

--- client/src/App.js
+++ client/src/App.js
@@ -15,16 +15,24 @@ class App extends Component {
 
   render() {
     return (
+
       <div className="App">
+
         <a id="saved-trips" href="#">Saved Trips</a>
 
         <div className="container">
+
           <form id="project-form" onSubmit={this.projectName}>
-            <p className="project-name-header">Give your trip a name...</p>
+            <p id="project-name-header">Name your trip</p>
             <input autofocus="autofocus" style={{ fontSize: "50px" }} id="project-name" type="text" required />
           </form>
+
           <img id="logo" src="https://www.shareicon.net/download/2015/09/13/100454_map_512x512.png" />
+
         </div>
+
+
+
 
       </div>
     );
