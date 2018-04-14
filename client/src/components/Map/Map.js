bc855138e46df06988e7a5a254c7465901df03ca
Josh Bay <joshbay@Joshs-MacBook-Pro.local>
2018-04-14 15:27:51 -0500

    added react-google-maps package

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,1 +1,9 @@
 import "./Map.css";
+import React, { Component } from "react";
+
+class Map extends Component {
+  render() {
+    return <div>Map Template</div>;
+  }
+}
+export default Map;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 import DashboardBackground from "../components/DashboardBackground";
 import { Redirect } from "react-router-dom";
+import Map from "../components/Map";
 
 class Dashboard extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            fireRedirect: false
-        };
-    }
-    render() {
-        return (
-            <DashboardBackground>
-                <div className="dashboard">
-                    <a id="saved-trips" href="#">
-                        Saved Trips
-                    </a>
-                </div>
-            </DashboardBackground>
-        )
-    }
-
+    this.state = {
+      fireRedirect: false
+    };
+  }
+  render() {
+    return (
+      <DashboardBackground>
+        <div className="dashboard">
+          <a id="saved-trips" href="#">
+            Saved Trips
+          </a>
+          <Map />
+        </div>
+      </DashboardBackground>
+    );
+  }
 }
-
 
 export default Dashboard;
