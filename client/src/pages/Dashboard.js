6b006e601694b41f5d8547565047e8659bcbaedf
Josh Bay <joshbay@Joshs-MacBook-Pro.local>
2018-04-16 11:48:30 -0500

    changed class to className

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -19,53 +19,117 @@ class Dashboard extends Component {
     return (
       <div>
         <DashboardBackground />
-        <h1 style={{ margin: `0`, color: `white`, textAlign: `center`, marginBottom: `40px` }}>Dashboard</h1>
+        <h1
+          style={{
+            margin: `0`,
+            color: `white`,
+            textAlign: `center`,
+            marginBottom: `40px`
+          }}
+        >
+          Dashboard
+        </h1>
         <div className="dashboard">
-
-
-          <div id="weather-map" style={{ display: `flex`, flexDirection: `row`, alignItems: `center`, justifyContent: `space-around` }}>
-            <div id="map-card" style={{ width: `45vw`, alignSelf: `flex-start`, height: `66vh`, margin: `0`, borderRadius: `25px` }} class="card blue-grey darken-1" >
-              <div class="card-content white-text">
-                <span class="card-title" style={{ textAlign: `center` }}>Map</span>
-                <div id="map-card-content" style={{ display: `flex`, justifyContent: `center`, alignItems: `center`, marginTop: `20px` }}>
-                  <Map /></div>
-
+          <div
+            id="weather-map"
+            style={{
+              display: `flex`,
+              flexDirection: `row`,
+              alignItems: `center`,
+              justifyContent: `space-around`
+            }}
+          >
+            <div
+              id="map-card"
+              style={{
+                width: `45vw`,
+                alignSelf: `flex-start`,
+                height: `66vh`,
+                margin: `0`,
+                borderRadius: `25px`
+              }}
+              className="card blue-grey darken-1"
+            >
+              <div className="card-content white-text">
+                <span className="card-title" style={{ textAlign: `center` }}>
+                  Map
+                </span>
+                <div
+                  id="map-card-content"
+                  style={{
+                    display: `flex`,
+                    justifyContent: `center`,
+                    alignItems: `center`,
+                    marginTop: `20px`
+                  }}
+                >
+                  <Map />
+                </div>
               </div>
             </div>
 
-
-
-            <div id="weather" style={{ width: `45vw`, alignSelf: `flex-start`, margin: `0`, borderRadius: `25px` }} class="card blue-grey darken-1" >
-              <div class="card-content white-text">
-                <span class="card-title" style={{ textAlign: `center`, color: `white` }}>Weather Forecast</span> <hr />
+            <div
+              id="weather"
+              style={{
+                width: `45vw`,
+                alignSelf: `flex-start`,
+                margin: `0`,
+                borderRadius: `25px`
+              }}
+              className="card blue-grey darken-1"
+            >
+              <div className="card-content white-text">
+                <span
+                  className="card-title"
+                  style={{ textAlign: `center`, color: `white` }}
+                >
+                  Weather Forecast
+                </span>{" "}
+                <hr />
                 <NOAAWeather />
               </div>
-              <div class="card-action" style={{
-                borderRadius: `25px`, textAlign: `center`
-              }}>
+              <div
+                className="card-action"
+                style={{
+                  borderRadius: `25px`,
+                  textAlign: `center`
+                }}
+              >
                 <a href="#">Captain's Log</a>
               </div>
             </div>
 
-
-
-
-
-            <div id="Dockwa" style={{ width: `45vw`, alignSelf: `flex-start`, margin: `0`, borderRadius: `25px` }} class="card blue-grey darken-1" >
-              <div class="card-content white-text">
-                <span class="card-title" style={{ textAlign: `center`, color: `white` }}>Docks in Your Area</span> <hr />
+            <div
+              id="Dockwa"
+              style={{
+                width: `45vw`,
+                alignSelf: `flex-start`,
+                margin: `0`,
+                borderRadius: `25px`
+              }}
+              className="card blue-grey darken-1"
+            >
+              <div className="card-content white-text">
+                <span
+                  className="card-title"
+                  style={{ textAlign: `center`, color: `white` }}
+                >
+                  Docks in Your Area
+                </span>{" "}
+                <hr />
                 <Dockwa />
               </div>
-              <div class="card-action" style={{
-                borderRadius: `25px`, textAlign: `center`
-              }}>
-              </div>
+              <div
+                className="card-action"
+                style={{
+                  borderRadius: `25px`,
+                  textAlign: `center`
+                }}
+              />
             </div>
           </div>
         </div>
       </div>
-
-
     );
   }
 }
