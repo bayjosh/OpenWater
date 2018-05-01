dcf5ce8c48a57652283288c7c5921047fdb6d88e
Seth Feder <sethfeder2017@u.northwestern.edu>
2018-04-30 21:40:58 -0500

    changed redirects from <Link/> to functions

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,48 @@
 import React, { Component } from "react";
 import HomeBackground from "../components/NameBackground";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 class Home extends Component {
   constructor() {
     super();
+    this.state = {
+      loginRedirect: false,
+      continueRedirect: false
+    }
   }
-
+  continue = event => {
+    event.preventDefault();
+    setTimeout(() => {
+      this.setState({ continueRedirect: true })
+    }, 500);
+  }
+  logIn = event => {
+    event.preventDefault();
+    setTimeout(() => {
+      this.setState({loginRedirect: true})
+    }, 500);
+  }
   render() {
 
     return (
       <div>
         <HomeBackground />
         <div className="home">
-          <div className="container">
+          <div className="container home-content">
             <p id="homeHeader">Start your voyage</p>
-            <Link to="/dashboard"><button
-              className="waves-effect waves-light btn"
+            <button
+              onClick={this.logIn}
+              className="waves-effect btn-large waves-light btn"
+              id="loginButton">Log In</button>
+            <button
+              onClick={this.continue}
+              className="waves-effect btn-large waves-light btn"
               id="continueAsGuestButton"
-              type="button"> Bon Voyage!</button></Link>
-            <Link to="/login"><button
-              className="waves-effect waves-light btn"
-              id="login"> Log In</button></Link>
+              type="button">Continue As Guest</button>
+
+            {this.state.loginRedirect && <Redirect to="/login" />}
+            {this.state.continueRedirect && <Redirect to="/dashboard" />}
+
           </div>
         </div>
       </div>
