2168cfd04926196322da06f5646e3e793554b986
Josh Bay <joshbay@Joshs-MacBook-Pro.local>
2018-04-30 19:44:35 -0500

    added register page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+
 
 
 const App = () => (
@@ -14,6 +16,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/register" component={Register} />
       {/* <Footer /> */}
     </div>
   </Router>
